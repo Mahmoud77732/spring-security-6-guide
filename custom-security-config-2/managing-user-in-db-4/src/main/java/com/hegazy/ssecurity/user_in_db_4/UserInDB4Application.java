4f2829a5cea32ffeb7614cc69648f8d33c6384a5
mahmoud77732 <mm8871918@gmail.com>
2025-06-10 11:39:45 +0300

    Define users in DB in default configs

---
+++ custom-security-config-2/managing-user-in-db-4/src/main/java/com/hegazy/ssecurity/user_in_db_4/UserInDB4Application.java
@@ -0,0 +1,13 @@
+package com.hegazy.ssecurity.user_in_db_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserInDB4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserInDB4Application.class, args);
+	}
+
+}
